67bcdc93d103d1112a3622ce9c696a199a2e86a7
Melvin Colmenares <melvin10tech@gmail.com>
2026-06-08 21:26:48 -0400

    Feat: Code formatting

--- src/contexts/shared/domain/repositories/auth/user.repository.interface.ts
+++ src/contexts/shared/domain/repositories/auth/user.repository.interface.ts
@@ -9,12 +9,22 @@ export interface IAuthProviderRecord {
 
 export interface IUserRepository extends IBaseRepository<UserEntity, string> {
   findByEmail(email: string): Promise<UserEntity | null>;
-  findByProviderUserId(providerId: string, providerUserId: string): Promise<UserEntity | null>;
+  findByProviderUserId(
+    providerId: string,
+    providerUserId: string,
+  ): Promise<UserEntity | null>;
   createWithProvider(
     userData: Partial<UserEntity>,
     providerId: string,
     providerUserId: string,
   ): Promise<UserEntity>;
-  findAuthProvider(providerId: string, providerUserId: string): Promise<IAuthProviderRecord | null>;
-  addAuthProvider(providerId: string, providerUserId: string, userId: string): Promise<void>;
+  findAuthProvider(
+    providerId: string,
+    providerUserId: string,
+  ): Promise<IAuthProviderRecord | null>;
+  addAuthProvider(
+    providerId: string,
+    providerUserId: string,
+    userId: string,
+  ): Promise<void>;
 }
